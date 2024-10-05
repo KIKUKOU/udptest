fe77d29bcf96255159e72777fd3bf106e292631f
KIKUKOU <baseballsoul.no3@gmail.com>
2024-10-06 07:40:07 +0900

    feat: add send port select and change send data (time to datetime).

--- recv.py
+++ recv.py
@@ -1,18 +1,15 @@
 import argparse
-import time
 import socket
 
 parser = argparse.ArgumentParser(description='UDP通信のテストプログラム（受信側）')
-parser.add_argument('-p', '--port', type=int, default=8080, help='送信側のポート（デフォルトは8080）')
-parser.add_argument('-t', '--time', action='store_true', help='受信時刻の表示')
+parser.add_argument('-p', '--port', type=int, default=8080, help='受信するポート（デフォルトは8080）')
 args = parser.parse_args()
 port = args.port
-recv_time_flag = args.time
 
 # ipv4を使うので、AF_INET
 # udp通信を使いたいので、SOCK_DGRAM
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(0.2)
+sock.settimeout(0.1)
 # ブロードキャストするときは255.255.255.255と指定せずに空文字
 sock.bind(('', port))
 try:
@@ -23,9 +20,7 @@ try:
         except socket.timeout:
             continue
 
-        print('send : {}  from {}'.format(rcv_data.decode(), addr))
-        if recv_time_flag:
-            print('recv : {}'.format(time.time()))
+        print('recv : {}  from {} to port ({})'.format(rcv_data.decode(), addr, port))
 
 except KeyboardInterrupt:
     sock.close()
--- send.py
+++ send.py
@@ -1,28 +1,39 @@
 import argparse
+import datetime
 import time
 import socket
 
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+
 parser = argparse.ArgumentParser(description='UDP通信のテストプログラム（送信側）')
-parser.add_argument('-H', '--host', default='<broadcast>', help='送信側のホストIP（デフォルトはbroadcast）')
-parser.add_argument('-p', '--port', type=int, default=8080, help='送信側のポート（デフォルトは8080）')
+parser.add_argument('-H', '--host', default='<broadcast>', help='送信元のホストIP（デフォルトはbroadcast）')
+parser.add_argument('-P', '--port', type=int, default=-1, help='送信元のポート（デフォルトは設定なし）')
+parser.add_argument('-p', '--dport', type=int, default=8080, help='送信先のポート（デフォルトは8080）')
 parser.add_argument('-n', '--num', type=int, default=1, help='送信回数（デフォルトは1）')
 parser.add_argument('-i', '--interval', type=float, default=1.0, help='送信間隔[s]（デフォルトは1.0）')
 args = parser.parse_args()
 host = args.host
-port = args.port
+send_port = args.port
+distination_port = args.dport
 num = args.num
 interval = args.interval
 
-# ipv4を使うので、AF_INET
-# udp通信を使いたいので、SOCK_DGRAM
+# ipv4での通信⇒AF_INET
+# udp通信⇒SOCK_DGRAM
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# ブロードキャストを行うので、設定
+# ブロードキャストの設定
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+# 送信元のポート設定
+if send_port != -1:
+    print(send_port)
+    sock.bind(('', send_port))
+
 # データ送信
 for n in range(num):
-    t = str(time.time())
+    t = now = datetime.datetime.now(JST).strftime('%x %X.%f %z(%Z)')
     print('send: {}'.format(t))
-    sock.sendto(t.encode(), (host, port))
+    sock.sendto(t.encode(), (host, distination_port))
     if n < num - 1:
         time.sleep(interval)
 
